7051029d631c315a0993e028b9979c0f049b315d
Justin Ryan Isidro <106646031+Crific@users.noreply.github.com>
2024-04-27 10:22:38 -0700

    Added timer

--- benchmark.cpp
+++ benchmark.cpp
@@ -93,13 +93,15 @@ int main(int argc, char** argv)
         memcpy((void *)Ycopy, (const void *)Y, sizeof(double)*n);
 
         // insert start timer code here
-
+        auto start_time = std::chrono::high_resolution_clock::now();
         // call the method to do the work
         my_dgemv(n, A, X, Y); 
 
         // insert end timer code here, and print out the elapsed time for this problem size
-
-
+        auto end_time = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end_time - start_time;
+        std::cout << "Elapsed time for problem size " << n << ": " 
+                  << elapsed.count() << " seconds" << std::endl;
         // now invoke the cblas method to compute the matrix-vector multiplye
         reference_dgemv(n, Acopy, Xcopy, Ycopy);
 
